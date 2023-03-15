0506a6b3c267e0b8102ad8f1553130c4c440dba8
Artur Azarov <artur.azarov@i4-insight.com>
2023-03-15 13:47:46 +0200

    make `find the best benefit test task`

---
+++ test.js
@@ -0,0 +1,36 @@
+const arrayOfNumbers = [15, 5, 4, 3, 2, 3];
+
+/**
+ *
+ * @param array - number[]
+ * @returns indexes for best benefits or empty array if there are no best benefits
+ */
+const findABenefit = (array) => {
+  const ascendingArraySort = array.slice().sort((a, b) => a - b);
+  let result = [];
+
+  for (i = 0; i <= array.length; i++) {
+    const descendingArraySortOfHigherNumbers = array
+      .filter((number) => ascendingArraySort[i] < number)
+      .sort((a, b) => b - a);
+
+    if (descendingArraySortOfHigherNumbers.length) {
+      descendingArraySortOfHigherNumbers.forEach((value) => {
+        const indexOfMinValue = array.indexOf(ascendingArraySort[i]);
+        const indexOfMaxValue = array.indexOf(value, indexOfMinValue);
+
+        if (indexOfMinValue < indexOfMaxValue && !result.length)
+          result = [
+            `min number ${ascendingArraySort[i]}, index: ${indexOfMinValue}`,
+            `max number ${value}, index: ${indexOfMaxValue}`,
+          ];
+      });
+
+      break;
+    }
+  }
+
+  return result;
+};
+
+console.log(findABenefit(arrayOfNumbers));
